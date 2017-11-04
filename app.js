840b648077544546188c3160a87dc161aaaddb8a
JTranie <julien.tranie@sciencespo.fr>
2017-11-04 09:40:49 +0100

    Changements fin de cours

--- app.js
+++ app.js
@@ -1,37 +1,40 @@
 var app = new Vue({
   el: '#app',
   data: {
-    firstName: "Jules",
-    lastName: "Bonnard",
-    description: "Je suis datajournaliste à l'AFP et j'enseigne le code, le traitement, et la visualisation de données à Sciences Po.",
-    photo: "https://pbs.twimg.com/profile_images/899285838516936704/nfeLD7he_400x400.jpg",
-    twitter: "julesbonnard",
-    github: "julesbonnard",
-    mail: "julesbonnard@gmail.com",
+    firstName: "Julien",
+    lastName: "Tranié",
+    description: "Journaliste web à La Croix.fr / #Nouveaux Formats",
+    photo: "https://pbs.twimg.com/profile_images/645226867360964608/oZU2pmoP.jpg",
+    twitter: "@JTranie",
+    github: "JTranie",
+    mail: "julien.tranie@gmail.com",
     experiences: [{
-      dateBegin: "2014",
+      dateBegin: "2017",
       dateEnd: "Today",
-      name: "Agence France Presse",
-      title: "Datajournalist",
+      name: "La Croix.fr / Bayard",
+      title: "Journaliste Web",
+      logo: "https://static.aws.la-croix.com/vb903611/bundles/lacroixsite/images/logo.svg",
       description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
     }, {
-      dateBegin: "Été 2013",
+      dateBegin: "Janvier 2016",
       dateEnd: false,
-      name: "Le Monde.fr",
-      title: "Data-journalist",
+      name: "RCF Haute Normandie",
+      title: "Aniamteur/Reporter radio",
+      logo: false,
       description: false
     }, {
-      dateBegin: "2012",
-      dateEnd: "2013",
-      name: "Le Dauphiné Libéré",
-      title: "Stagiaire et correspondant de presse",
+      dateBegin: "2016",
+      dateEnd: false,
+      name: "Les Echos",
+      title: "Stagiaire au service >Services<",
+      logo: false,
       description: false
     }],
     formations: [{
-      dateBegin: "2012",
-      dateEnd: "2014",
+      dateBegin: "2015",
+      dateEnd: "2018",
       name: "Master Journalisme",
-      university: "Sciences Po Grenoble"
+      university: "Ecole de journalisme de Sciences Po"
     }, {
       dateBegin: "2009",
       dateEnd: "2012",
